763eb02fa0c0453dc69539c69f83ebe33944448a
zhaibo <729963917@qq.com>
2019-10-08 18:39:25 +0800

    create monitor module.

---
+++ monitor/zipkin-server/src/main/java/ink/zhaibo/ms/zipkin/server/ZipkinServerApplication.java
@@ -0,0 +1,13 @@
+package ink.zhaibo.ms.zipkin.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZipkinServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZipkinServerApplication.class, args);
+	}
+
+}
