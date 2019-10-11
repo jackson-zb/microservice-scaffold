9e15caa71f8374dcc0a8fb28e221bced6cd33c10
zhaibo <729963917@qq.com>
2019-10-11 10:37:43 +0800

    项目结构调整.

---
+++ biz/user-api/src/main/java/ink/zhaibo/ms/user/api/UserApiApplication.java
@@ -0,0 +1,13 @@
+package ink.zhaibo.ms.user.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserApiApplication.class, args);
+	}
+
+}
---
+++ config/src/main/java/ink/zhaibo/ms/config/ConfigApplication.java
@@ -0,0 +1,13 @@
+package ink.zhaibo.ms.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigApplication.class, args);
+	}
+
+}
