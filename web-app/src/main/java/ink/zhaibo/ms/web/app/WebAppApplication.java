27bfb14119bbf086f58a0ad8824e7cc4c83f101a
zhaibo <729963917@qq.com>
2019-10-17 10:22:03 +0800

    整合actuator.

--- web-app/src/main/java/ink/zhaibo/ms/web/app/WebAppApplication.java
+++ web-app/src/main/java/ink/zhaibo/ms/web/app/WebAppApplication.java
@@ -2,14 +2,16 @@ package ink.zhaibo.ms.web.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * @Author zhaibo
  * @Date 2019/10/16 18:04
  **/
-@EnableEurekaClient
+@EnableDiscoveryClient
 @SpringBootApplication
+@EnableFeignClients
 public class WebAppApplication {
     public static void main(String[] args) {
         SpringApplication.run(WebAppApplication.class, args);
