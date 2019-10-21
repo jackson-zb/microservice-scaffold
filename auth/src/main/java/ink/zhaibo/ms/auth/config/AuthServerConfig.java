dad2eacbc43d2f6e40e274b974a57b1da7e95075
zhaibo <729963917@qq.com>
2019-10-21 16:07:58 +0800

    add spring-cloud-gateway.

--- auth/src/main/java/ink/zhaibo/ms/auth/config/AuthServerConfig.java
+++ auth/src/main/java/ink/zhaibo/ms/auth/config/AuthServerConfig.java
@@ -1,50 +1,11 @@
 package ink.zhaibo.ms.auth.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
-import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
-import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 
 @EnableAuthorizationServer
 @Configuration
 public class AuthServerConfig extends AuthorizationServerConfigurerAdapter {
-    @Autowired
-    private AuthenticationManager authenticationManager;
 
-    @Autowired
-    private BCryptPasswordEncoder bCryptPasswordEncoder;
-
-    @Autowired
-    private UserDetailsService userDetailsService;
-
-    @Override
-    public void configure(AuthorizationServerSecurityConfigurer oauthServer) throws Exception {
-        oauthServer.realm("oauth2-resources")
-                .tokenKeyAccess("permitAll()").checkTokenAccess("isAuthenticated()")
-                .allowFormAuthenticationForClients();
-    }
-
-    @Override
-    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-        endpoints.authenticationManager(authenticationManager)
-                // 允许 GET、POST 请求获取 token，即访问端点：oauth/token
-                .allowedTokenEndpointRequestMethods(HttpMethod.GET, HttpMethod.POST);
-        // 要使用refresh_token的话，需要额外配置userDetailsService
-        endpoints.userDetailsService(userDetailsService);
-    }
-
-    @Override
-    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-        clients.inMemory().withClient("demoApp").secret(bCryptPasswordEncoder.encode("demoAppSecret"))
-                .authorizedGrantTypes("authorization_code", "client_credentials", "password", "refresh_token")
-                .scopes("all").resourceIds("oauth2-resource").accessTokenValiditySeconds(1200)
-                .refreshTokenValiditySeconds(50000);
-    }
 }
