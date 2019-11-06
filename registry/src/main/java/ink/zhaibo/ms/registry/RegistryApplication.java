30176f5171cc4eb0e8e17c4576629396d208fde6
zhaibo <729963917@qq.com>
2019-11-06 19:23:13 +0800

    错误码修改.

--- registry/src/main/java/ink/zhaibo/ms/registry/RegistryApplication.java
+++ registry/src/main/java/ink/zhaibo/ms/registry/RegistryApplication.java
@@ -3,10 +3,11 @@ package ink.zhaibo.ms.registry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-//@EnableHystrixDashboard
+@EnableHystrixDashboard
 public class RegistryApplication {
     public static void main(String[] args) {
         SpringApplication.run(RegistryApplication.class, args);
