cd9e46c18eb31e4911e8fd05868272c69dbe241e
zhaibo <729963917@qq.com>
2019-11-26 20:55:20 +0800

    WebSecurityConfig

---
+++ auth/src/main/java/ink/zhaibo/ms/auth/config/WebSecurityConfig.java
@@ -0,0 +1,21 @@
+package ink.zhaibo.ms.auth.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @author zhaibo
+ */
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.authorizeRequests().antMatchers(
+                new String[]{"/actuator/**", "/auth/**"}
+        ).permitAll().anyRequest().authenticated();
+    }
+}
